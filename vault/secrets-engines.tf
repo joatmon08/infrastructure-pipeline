7eefb65045db94e00da35307b3473f5d7fcbb50f
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-11-10 13:59:20 -0500

    Update to reference generic backend

--- vault/secrets-engines.tf
+++ vault/secrets-engines.tf
@@ -4,8 +4,8 @@ resource "vault_mount" "pipeline" {
   description = "For infrastructure-pipeline static secrets"
 }
 
-resource "vault_generic_secret" "pipeline_boostrap" {
-  path = "${vault_mount.pipeline.path}/bootstrap"
+resource "vault_generic_secret" "pipeline" {
+  path = "${vault_mount.pipeline.path}"
 
   data_json = <<EOT
 {
