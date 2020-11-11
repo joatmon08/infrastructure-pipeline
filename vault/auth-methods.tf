1eb61507e42d5bcefb0f238e85aa3c5c0e53c529
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-11-11 17:09:08 -0500

    Update role ID with application pipeline

--- vault/auth-methods.tf
+++ vault/auth-methods.tf
@@ -27,6 +27,8 @@ resource "vault_auth_backend" "approle" {
 resource "vault_approle_auth_backend_role" "pipeline" {
   backend            = vault_auth_backend.approle.path
   role_name          = var.pipeline_name
+  role_id            = var.pipeline_name
+  secret_id_ttl      = 86400
   secret_id_num_uses = 0
   token_ttl          = 600
   token_max_ttl      = 3600
